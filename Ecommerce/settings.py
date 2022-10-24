1df7ecc28475f40ee95b1244e3e5838e91585c58
GitSten <stensagar@gmail.com>
2022-10-24 20:53:53 +0300

    finished version 1.0

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o)6bx+1vvpn%^3au=auh$-u=f_42onn$zsrcmwb1ri3uuk3sg$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 # Application definition
 
